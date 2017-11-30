c79e5cb914cf74993ac8d3c014810f59368ee184
armasyll <armasyll@gmail.com>
2017-11-30 10:02:02 -0500

    Commented

--- js/Start.js
+++ js/Start.js
@@ -1,12 +1,13 @@
-/*
-    Set up the GUI, and Start.
-*/
+// Pre-load entity images
 entityIndexes.forEach(function(_entity) {
     if (_entity.hasOwnProperty("image") && _entity.image !== undefined) {
         var _img = new Image();
         _img.src = _entity.image;
     }
 });
+/*
+    Set up the GUI, and Start.
+*/
 Menu.initialize();
 resizeGui();
 mainMenu();
