347e86e7111d738ba91ad2c6e5a0a1eb19b076cf
armasyll <armasyll@gmail.com>
2017-12-03 09:17:44 -0500

    Trimming

--- js/bootstrapStuff.js
+++
@@ -1,7 +0,0 @@
-$(document).on("click", ".viewPersonalInventory", function(event){
-    event.preventDefault();
-    
-    $("#personalInventoryModal").modal("show");
-    $('#personalInventoryModal-title').html("<img style='height:2em' src='{0}' alt=''/>Your Inventory".format(player.image));
-    $('#personalInventoryModal-body').html(generateEntityItemsGraphicalList(player, undefined, false));
-});
